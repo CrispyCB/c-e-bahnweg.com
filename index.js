0a5b73c9f63630f7550c6d69e3db6a9378af10b0
CrispyCB <cebahnweg@gmail.com>
2017-12-11 22:01:32 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+$("#who").click(function()
+ {$("#whocolumns").slideToggle();});
+$("#does").click(function()   {$("#doescolumns").slideToggle();});
+$("#made").click(function()
+ {$("#madedivs").slideToggle();});
+$("#web").click(function()
+{$("#socialmedia").slideToggle();});
+$("#coding").click(function()
+{$("#github-projects").slideToggle();});
+$("#writing").click(function()
+{$("#writing-samples").slideToggle();});
+});            
